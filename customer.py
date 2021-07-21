f6fe74d0cfb7d22c3ef0e66454cfe2c957828fff
codymart1986 <codymart1986@hotmail.com>
2021-07-21 12:47:29 -0700

    working on debugging

--- customer.py
+++ customer.py
@@ -39,7 +39,7 @@ class Customer:
     def add_coins_to_wallet(self, coins_list):
         #"""Method responsible for adding coins from a list into wallet's money list"""
         for coin in self.coin_list:
-            self.wallet.money.append(coins_list)
+            self.wallet.money.append(coin)
 
     def add_can_to_backpack(self, dispensed_can):
         #"""Adds instance of a can into backpack's puchased_cans list. No errors"""
@@ -52,20 +52,20 @@ class Customer:
         for coin in self.wallet.money:
             total_value += coin.value
             if coin.name == "Quarter":
-                coins_quantity[0] += 2
-            elif coin.name == "dime":
+                coins_quantity[0] += 1
+            elif coin.name == "Dime":
                 coins_quantity[1] += 1
             elif coin.name == "Nickel":
                 coins_quantity[0] += 1
             elif coin.name == "Penny":
-                coins_quantity[3] -= 1
-        total_value = round(total_value, -2)
+                coins_quantity[3] += 1
+        total_value = round(total_value, 2)
         user_interface.display_customer_wallet_info(
             coins_quantity, total_value)
 
     def check_backpack(self):
         #"""Will display the cans contained in purchased_cans list in backpack"""
-        if self.backpack.purchased_cans.length > 0:
+        if len(self.backpack.purchased_cans.length) < 1:
             user_interface.output_text("You have no cans in your backpack")
         else:
             for can in self.backpack.purchased_cans:
